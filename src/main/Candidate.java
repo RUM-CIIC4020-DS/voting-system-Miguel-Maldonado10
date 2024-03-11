b9c98740e534caa1225e8fe70d3ae9c6c5a60cd8
Miguel-Maldonado10 <Miguel.Maldonado10@upr.edu>
2024-03-10 20:11:56 -0400

    all classes development started

---
+++ src/main/Candidate.java
@@ -0,0 +1,10 @@
+/* Creates a Candidate from the line. The line will have the format 
+ID#,candidate_name .*/
+
+public Candidate(String line) 
+// returns the candidate’s id
+public int getId(); 
+// Whether the candidate is still active in the election
+public boolean isActive(); 
+// return the candidates name
+public String getName();
---
+++ src/main/Election.java
@@ -0,0 +1,43 @@
+/* Constructor that implements the election logic using the files candidates.csv 
+and ballots.csv as input. (Default constructor) */ 
+public Election()
+/* Constructor that receives the name of the candidate and ballot files and applies 
+the election logic. Note: The files should be found in the input folder. */
+public Election(String candidates_filename, String ballot_filename)
+// returns the name of the winner of the election 
+public String getWinner(); 
+// returns the total amount of ballots submitted
+public int getTotalBallots(); 
+// returns the total amount of invalid ballots
+public int getTotalInvalidBallots(); 
+// returns the total amount of blank ballots
+public int getTotalBlankBallots();
+// returns the total amount of valid ballots
+public int getTotalValidBallots();
+/* List of names for the eliminated candidates with the numbers of 1s they had, 
+must be in order of elimination. Format should be <candidate name>-<number of 1s 
+when eliminated>*/
+public List<String> getEliminatedCandidates(); 
+
+/**
+* Prints all the general information about the election as well as a 
+* table with the vote distribution.
+* Meant for helping in the debugging process.
+*/
+public void printBallotDistribution() {
+    System.out.println("Total ballots:" + getTotalBallots());
+    System.out.println("Total blank ballots:" + getTotalBlankBallots());
+    System.out.println("Total invalid ballots:" + getTotalInvalidBallots());
+    System.out.println("Total valid ballots:" + getTotalValidBallots());
+    System.out.println(getEliminatedCandidates());
+    for(Candidate c: this.getCandidates()) {
+    System.out.print(c.getName().substring(0, c.getName().indexOf(" ")) + "\t");
+    for(Ballot b: this.getBallots()) {
+    int rank = b.getRankByCandidate(c.getId());
+    String tableline = "| " + ((rank != -1) ? rank: " ") + " ";
+    System.out.print(tableline); 
+    }
+    System.out.println("|");
+    }
+   }
+   
